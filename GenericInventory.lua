3373ae54e254ff0779c42a10c157baacef5deb38
ZachCurtis <31259055+ZachCurtis@users.noreply.github.com>
2017-11-02 05:13:59 -0400

    Create GenericInventory.lua

---
+++ GenericInventory.lua
@@ -0,0 +1,48 @@
+local Inventory = {}
+
+if not _G.inv then 
+    _G.inv = {}
+end
+
+function Inventory:AddPlayer(player,loadData)
+	--loadData coming from datastore
+	if loadData then
+		_G.inv[player] = loadData
+	else
+		_G.inv[player] = {}
+	end
+end
+
+function Inventory:GetInventory(player)
+	return _G.inv[player]
+end
+
+function Inventory:AddItem(player, itemToAdd)
+	table.insert(_G.inv[player], itemToAdd)
+end
+
+function Inventory:RemoveItem(player, itemToRemove)
+	for i = 1, #_G.inv[player] do
+		if _G.inv[player][i] == itemToRemove then
+			table.remove(_G.inv[player], i)
+			break
+		end
+	end
+end
+
+function Inventory:CheckOwned(player, itemToCheck)
+	local check = false
+	for i = 1, #_G.inv[player] do
+		if _G.inv[player][i] == itemToCheck then
+			check = true
+			break
+		end
+	end
+	return check
+end
+
+function Inventory:RemovePlayer(player)
+	_G.inv[player] = nil	
+end
+
+return Inventory
